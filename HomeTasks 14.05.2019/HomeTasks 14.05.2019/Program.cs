fa83b02c5dbfa548dd92d5349786b0a49495c9fb
Kamal Guliyev <kamalag@code.edu.az>
2019-05-15 04:34:27 +0400

    Algorithmic tasks in Console Application

---
+++ HomeTasks 14.05.2019/HomeTasks 14.05.2019/Program.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeTasks_14._05._2019
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region HomeTask1
+            //string input = "abcd efgh qwer adadadasd dasdasd sdadasdasd asdasdas sadad";
+            //Console.WriteLine("Enter your sentence to find the number of spaces into it");
+            //string input = Console.ReadLine();
+            //string[] inputResult = input.Split(' ');
+            //FindTheNumberOfSpaces(inputResult);
+            #endregion
+
+            #region HomeTask2
+            //int[] input = { 1, 2, 3, 4, 5 };
+            //Console.WriteLine("Enter numbers by commas as you want");
+            //string input = Console.ReadLine();
+            //string[] input2 = input.Split(',');
+            //FindAverageNumber(input2);
+            #endregion
+
+            #region HomeTask3
+            //string input = Console.ReadLine();
+            //string[] input2 = input.Split(' ');
+            //GetArrayOfEvenNumbers(input2);
+            #endregion
+
+            #region HomeTask4
+            //string input = Console.ReadLine();
+            //string[] input2 = input.Split(',');
+            //FindElderThanAverageNumber(input2);
+            #endregion
+
+            #region HomeTask5
+            //string input = Console.ReadLine();
+            //string input2 = Console.ReadLine();
+            //int firstNumber = int.Parse(input);
+            //int secondNumber = int.Parse(input2);
+            //FindPowerOfNumber(firstNumber, secondNumber);
+            #endregion
+
+            #region HomeTask6
+            //string input = Console.ReadLine();
+            //string[] input2 = input.Split(' ');
+            //FindLongWord(input2);
+            #endregion
+
+            #region HomeTask7
+            //string input = Console.ReadLine();
+            //string[] input2 = input.Split(',');
+            //ConcatWords(input2);
+            #endregion
+
+        }
+
+        #region HomeTask1
+        //static void FindTheNumberOfSpaces(string[] sentence)
+        //{
+        //    int spacesNumber = sentence.Length - 1;
+        //    Console.WriteLine(spacesNumber);
+        //    Console.ReadLine();
+        //}
+        #endregion
+
+        #region HomeTask2
+        //static void FindAverageNumber(string[] numbers)
+        //{
+        //    int total = 0;
+        //    double result = 0;
+        //    for (int i = 0; i < numbers.Length; i++)
+        //    {
+        //        total += int.Parse(numbers[i]);
+        //        result = total / numbers.Length;
+        //    }
+        //    Console.WriteLine(result);
+        //    Console.ReadLine();
+        //}
+        #endregion
+
+        #region HomeTask3
+        //static void GetArrayOfEvenNumbers(string[] numbers)
+        //{
+        //    string result = "";
+        //    for (int i = 0; i < numbers.Length; i++)
+        //    {
+        //        int number = int.Parse(numbers[i]);
+
+        //        if (number % 2 == 0)
+        //        {
+        //            result += number + " ";
+
+        //            //for (int j = 0; j < result.Length; j++)
+        //            //{
+        //            //    if(result[result.Length - 1] == ',')
+        //            //    {
+        //            //        result[result.Length - 1] = '.';
+        //            //    }
+        //            //}
+
+        //        }
+
+        //    }
+        //    Console.Write(result);
+        //    Console.ReadLine();
+        //}
+        #endregion
+
+        #region HomeTask4
+        //static void FindElderThanAverageNumber(string[] numbers)
+        //{
+        //    int total = 0;
+        //    int average = 0;
+        //    for (int i = 0; i < numbers.Length; i++)
+        //    {
+        //        int number = int.Parse(numbers[i]);
+        //        total += number;
+        //        average = total / numbers.Length;
+
+
+        //    }
+
+        //    for (int j = 0; j < numbers.Length; j++)
+        //    {
+        //        int number = int.Parse(numbers[j]);
+        //        if (number > average)
+        //        {
+        //            Console.WriteLine(number);
+        //        }
+        //    }
+
+        //    Console.ReadLine();
+        //}
+        #endregion
+
+        #region HomeTask5
+        //static void FindPowerOfNumber(int number1,int number2)
+        //{
+        //    int number = number1;
+        //    for (int i = 1; i < number2; i++)
+        //    {
+        //        number1 = number * number1;
+        //    }
+
+        //    Console.WriteLine(number1);
+        //    Console.ReadLine();
+        //}
+        #endregion
+
+        #region HomeTask6
+        //static void FindLongWord(string[] words)
+        //{
+        //    string longWord = " ";
+        //    int length = words[0].Length;
+
+        //    for (int i = 0; i < words.Length; i++)
+        //    {
+        //        if (length < words[i].Length)
+        //        {
+        //            length = words[i].Length;
+        //            longWord = words[i];
+        //        }
+        //        else
+        //        {
+        //            longWord = words[0];
+        //        }
+        //    }
+        //    Console.Write(longWord);
+
+        //    Console.ReadLine();
+        //}
+        #endregion
+
+        #region HomeTask7
+        //static void ConcatWords(string[] words)
+        //{
+        //    //string result = "";
+        //    //string result2 = "";
+        //    for (int i = 0; i < words.Length; i++)
+        //    {
+        //        //result += words[i];
+        //        //result2 = result + ",";
+        //        Console.Write(words[i] + ',');
+        //    }
+
+        //    Console.ReadLine();
+        //}
+        #endregion
+    }
+}
---
+++ P110_ConsoleDemo/P110_ConsoleDemo/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Text;
+
+namespace P110_ConsoleDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Try Parse
+            //Console.Write("Reqem daxil edin:");
+            //string demo = Console.ReadLine();
+            //int kamal;
+
+            //bool hasSucceeded = int.TryParse(demo, out kamal);
+
+            //while (!hasSucceeded)
+            //{
+            //    Console.Write("Duzgun reqem daxil edin:");
+            //    demo = Console.ReadLine();
+            //    hasSucceeded = int.TryParse(demo, out kamal);
+            //}
+            #endregion
+
+            #region Classwork reverse sentence by words
+            //Console.WriteLine("Please, input a sentence:");
+            //string userInput = Console.ReadLine();
+            //string result = "";
+            ////salam samir qaqa
+            ////malas rimas aqaq
+
+            ////1. Split sentence into words
+            //string[] words = userInput.Split(' ');
+
+            ////2. loop words in order
+            //for (int i = 0; i < words.Length; i++)
+            //{
+            //    //3. loop in word in reverse order
+            //    string word = words[i];
+            //    for (int j = word.Length - 1; j >= 0; j--)
+            //    {
+            //        result += word[j];
+            //    }
+            //    result += " ";
+            //}
+
+            //Console.WriteLine(result);
+            #endregion
+
+            #region Array resize
+            //long currentReservedMemory = GC.GetTotalMemory(false);
+            //Console.WriteLine(currentReservedMemory);
+
+            //int[] numbers = new int[5];
+            //numbers[0] = 15;
+            //numbers[1] = 25;
+            //numbers[2] = 5;
+            //numbers[3] = 3;
+            //numbers[4] = 7;
+
+            //Array.Resize(ref numbers, 6);
+            #endregion
+
+            #region Char
+            //char letterAInt = 'a';
+            //Console.WriteLine((int)letterAInt);
+
+            //for(char c = 'A'; c <= 'Z'; c++)
+            //{
+            //    Console.Write(c);
+            //    Console.Write(",");
+            //}
+
+            //Console.WriteLine(GetLetter(5));
+            #endregion
+
+            #region Strings
+
+            //Console.WriteLine("Salam" + ", " + " Samir");
+
+            //StringBuilder word = new StringBuilder();
+            //word.Append("12345678901234567");
+            //word.Remove(4, 3);
+
+            //Console.WriteLine(word.Length);
+            //Console.WriteLine(word.Capacity);
+            //Console.WriteLine(word);
+
+            int[] n = { 10, 20, 40 };
+            Sum(n);
+
+            #endregion
+        }
+
+        //static char GetLetter(int n)
+        //{
+        //    return (char)('a' + n - 1);
+        //}
+
+        static void Sum(int[] numbers)
+        {
+            for (int i = 0; i < numbers.Length; i++)
+			{
+                Console.WriteLine(numbers[i]);
+			}
+            Console.ReadLine();
+        }
+
+    }
+    
+}
